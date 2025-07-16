5817e0fc024f0131857bba78cd2bc16bb25a6ab2
amilton0656 <amilton0656@gmail.com>
2025-07-16 15:49:54 -0300

    inicio

---
+++ core/urls.py
@@ -0,0 +1,32 @@
+from django.contrib import admin
+from django.conf import settings
+from django.conf.urls.static import static
+from django.urls import path, include
+
+# No arquivo views.py do seu app Django
+import logging
+from django.http import JsonResponse
+
+# Obtenha o logger configurado
+logger = logging.getLogger('requestlogs')
+
+def test_logging_view(request):
+    try:
+        data = {'user': 'leticia', 'email': 'leticia@contato.com'}
+        # Força um erro proposital para testar o log
+        raise ValueError("Erro simulado no envio de email")
+    except Exception as e:
+        # Loga o erro usando o logger configurado
+        logger.error(f"{str(e)} | {str(data)}")
+        return JsonResponse({"status": "error", "message": "Ocorreu um erro."})
+
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    # path('test-logging/', test_logging_view, name='test_logging'),
+    path('', include('pages.urls')),
+]
+
+if settings.DEBUG: # update 03/11/2024: (em homologa com debug true adiciona rota static)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
