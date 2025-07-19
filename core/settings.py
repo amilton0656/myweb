c7e1f86e8e91b7d0bb58cb2dd0ed0aa5a32bff65
unknown <amilton0656@gmail.com>
2025-07-19 12:32:26 -0300

    xx

--- core/settings.py
+++ core/settings.py
@@ -222,7 +222,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/ 
 
 STATIC_ROOT = os.path.join(BASE_DIR,'static')
-STATIC_URL = '/static/' 
+STATIC_URL = 'static/' 
 
 
 # STATICFILES_DIRS = [ # talvez em Produção podesse usar assim.
