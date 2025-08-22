72b3b3bd372d2268d51ab717997c7ca1e1108a8f
amilton0656 <amilton0656@gmail.com>
2025-08-22 14:29:27 -0300

    xxx

--- core/settings.py
+++ core/settings.py
@@ -107,3 +107,6 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+STATICFILES_STORAGE = "django.contrib.staticfiles.storage.ManifestStaticFilesStorage"
+
